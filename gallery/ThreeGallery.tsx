613ba2b9541e79e59ac97817edc51c2e2662c4c2
unknown <2timesjay@gmail.com>
2023-01-21 23:42:16 -0500

    Stub out gallery

---
+++ gallery/ThreeGallery.tsx
@@ -0,0 +1,15 @@
+import '../src/App.css'
+
+function ThreeGallery() {
+  return (
+    <div className="ThreeGallery">
+      <div>
+        <a href="/gallery/" target="_blank">
+          <img src="/three.png" className="logo three" alt="ThreeJS logo" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default ThreeGallery
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        gallery: resolve(__dirname, 'gallery/index.html'),
+      },
+    },
+  },
+  plugins: [react()],
+})
